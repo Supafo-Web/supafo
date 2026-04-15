b76450ceea8806e308c029846b95d74afb2bff03
Tuncay Demir <tuncaydemir682@gmail.com>
2026-04-15 16:23:09 +0300

    updated

--- components/social-media/SocialMedia.tsx
+++ components/social-media/SocialMedia.tsx
@@ -16,23 +16,33 @@ const SocialButtons = () => {
    const socialLink = [
       {
          id: 1,
-         icon: '/socials/WhatsApp.svg', link: openWhatsApp
+         icon: '/socials/WhatsApp.svg',
+         icon2: '/socials/WhatsApp-white.svg',
+         link: openWhatsApp
       },
       {
          id: 2,
-         icon: '/socials/Instagram.svg', link: openInstagram
+         icon: '/socials/Instagram.svg',
+         icon2: '/socials/Instagram-white.svg',
+         link: openInstagram
       },
       {
          id: 3,
-         icon: '/socials/X.svg', link: openX
+         icon: '/socials/X.svg',
+         icon2: '/socials/X-white.svg',
+         link: openX
       },
       {
          id: 4,
-         icon: '/socials/Tiktok.svg', link: openTiktok
+         icon: '/socials/Tiktok.svg',
+         icon2: '/socials/Tiktok-white.svg',
+         link: openTiktok
       },
       {
          id: 5,
-         icon: '/socials/Discord.svg', link: openDiscord
+         icon: '/socials/Discord.svg',
+         icon2: '/socials/Discord-white.svg',
+         link: openDiscord
       },
    ]
 
@@ -44,16 +54,26 @@ const SocialButtons = () => {
             <Button
                key={item.id || index}
                onClick={item.link}
-               className="bg-white p-2.75 rounded-full cursor-pointer"
+               className="group cursor-pointer rounded-full bg-white p-2.75 transition-all duration-300 ease-in-out hover:bg-[#578B23] hover:scale-105"
             >
-               <Image
-                  alt="social"
-                  src={item.icon}
-                  width={24}
-                  height={24}
-                  priority
-                  style={{ width: 24, height: 24 }}
-               />
+               <div className="relative h-6 w-6">
+                  <Image
+                     alt="social"
+                     src={item.icon}
+                     width={24}
+                     height={24}
+                     priority
+                     className="absolute inset-0 opacity-100 transition-opacity duration-500 ease-in-out group-hover:opacity-0"
+                  />
+                  <Image
+                     alt="social hover"
+                     src={item.icon2}
+                     width={24}
+                     height={24}
+                     priority
+                     className="absolute inset-0 opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100"
+                  />
+               </div>
             </Button>
          ))}
       </div>
